1edb623327fc74ecb0947d956298779fcef97030
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-08-08 12:44:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+with open('files/books_descriptions.json', 'r', encoding='UTF-8') as file:
+    books = file.read()
+books = json.loads(books)
+
+rendered_page = template.render(
+    books=books,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
